54899d2b0c8c9b09e60ba32bd11b9c966655bf84
Luis <luibenmor@gmail.com>
2023-12-11 08:35:50 +0100

    1er ejercicio

---
+++ src/Func1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Func1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        double num1 = sc.nextDouble();
+        double num2 = sc.nextDouble();
+
+        double mult = multiplica(num1,num2);
+        System.out.println(mult);
+
+    }
+
+    public static double multiplica(double a, double b) {
+        double result = a * b;
+        return result;
+    }
+
+}
